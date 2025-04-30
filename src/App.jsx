562d93cb9049d56bbcfda64a2882edabd8795474
nomadriding <fundora.a@gmail.com>
2025-04-29 23:48:16 -0400

    begin project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import React from "react"
 import { useState } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./App.css"
 import Hangman from "./Hangman/Hangman.jsx"
+import HomePage from "./HomePage/HomePage.jsx"
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       <Route path="/Hangman" element={<Hangman />} />
     </Routes>
   )
--- src/Hangman/Hangman.jsx
+++ src/Hangman/Hangman.jsx
@@ -1,11 +1,19 @@
 import React from "react"
+import Layout from "../Layout"
+import "./Hangman.css"
 
 function Hangman() {
   return (
-    <div className="hangmanContainer">
-      <h1 className="GameTitle">Hangman</h1>
-      <p>Welcome to Hangman</p>
-    </div>
+    <Layout>
+      <div className="hangmanContainer">
+        <h1 className="GameTitle">Hangman</h1>
+        <p>Welcome to Hangman</p>
+      </div>
+      <div className="hangmanContainer">
+        <h1 className="GameTitle">Hangman</h1>
+        <p>Welcome to Hangman</p>
+      </div>
+    </Layout>
   )
 }
 export default Hangman
