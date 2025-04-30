63e8d2930f8ec46abd42fa0a06d6752da0243b2f
nomadriding <fundora.a@gmail.com>
2025-04-29 23:24:59 -0400

    prep the files with first project

---
+++ src/Hangman/Hangman.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Hangman() {
+  return (
+    <div className="hangmanContainer">
+      <h1 className="GameTitle">Hangman</h1>
+      <p>Welcome to Hangman</p>
+    </div>
+  )
+}
+export default Hangman
