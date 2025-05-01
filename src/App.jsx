e8245c4892b935e66ef525d253a894e978d9e2e3
nomadriding <fundora.a@gmail.com>
2025-04-30 21:29:35 -0400

    Fix path links

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./App.css"
 import Hangman from "./Hangman/Hangman.jsx"
 import HomePage from "./HomePage/HomePage.jsx"
+import Projects from "./Projects.jsx"
+import About from "./About.jsx"
+import Resume from "./Resume.jsx"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/Projects" element={<Projects />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Resume" element={<Resume />} />
+      <Route path="/Projects/Hangman" element={<Hangman />} />
       <Route path="/Hangman" element={<Hangman />} />
     </Routes>
   )
