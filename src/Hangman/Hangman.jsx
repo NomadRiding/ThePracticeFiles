984eedb6526c4ad13d731124c21ed550530d1e97
nomadriding <fundora.a@gmail.com>
2025-04-30 22:26:47 -0400

    Begin logic

--- src/Hangman/Hangman.jsx
+++ src/Hangman/Hangman.jsx
@@ -3,15 +3,33 @@ import Layout from "../Layout"
 import "./Hangman.css"
 
 function Hangman() {
+  // State variables for the game
+
+  const [wordList, setWordList] = React.useState([
+    "Tourist",
+    "Pineapple",
+    "Japan",
+    "Egypt",
+    "Roaming",
+  ])
+
+  function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * wordList.length)
+    return wordList[randomIndex].toLowerCase()
+  }
+
+  React.useEffect(() => {
+    const randomWord = getRandomWord()
+    setWord(randomWord)
+  }, [])
+  const [word, setWord] = React.useState("")
+
   return (
     <Layout>
       <div className="hangmanContainer">
         <h1 className="GameTitle">Hangman</h1>
-        <p>Welcome to Hangman</p>
-      </div>
-      <div className="hangmanContainer">
-        <h1 className="GameTitle">Hangman</h1>
-        <p>Welcome to Hangman</p>
+        <p className="GameDescription">A fun word guessing game!</p>
+        <p>Guess the word before you run out of attempts!</p>
       </div>
     </Layout>
   )
