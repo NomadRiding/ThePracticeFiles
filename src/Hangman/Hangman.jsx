67494080c0dfa3bb7be6100a7399d4b944fa2163
nomadriding <fundora.a@gmail.com>
2025-05-01 23:37:15 -0400

    adding some game logic

--- src/Hangman/Hangman.jsx
+++ src/Hangman/Hangman.jsx
@@ -3,9 +3,12 @@ import Layout from "../Layout"
 import "./Hangman.css"
 
 function Hangman() {
-  // State variables for the game
+  const [startGame, setStartGame] = React.useState(false)
+  const [gameOver, setGameOver] = React.useState(false)
+  const [gameWon, setGameWon] = React.useState(false)
+  const [showWord, setShowWord] = React.useState(false)
 
-  const [wordList, setWordList] = React.useState([
+  const [wordList] = React.useState([
     "Tourist",
     "Pineapple",
     "Japan",
@@ -13,16 +16,51 @@ function Hangman() {
     "Roaming",
   ])
 
+  const [word, setWord] = React.useState("")
+  const [guessedLetters, setGuessedLetters] = React.useState([])
+  const [attempts, setAttempts] = React.useState(6)
+  const [correctLetters, setCorrectLetters] = React.useState([])
+  const [wrongLetters, setWrongLetters] = React.useState([])
+
   function getRandomWord() {
     const randomIndex = Math.floor(Math.random() * wordList.length)
     return wordList[randomIndex].toLowerCase()
   }
 
   React.useEffect(() => {
-    const randomWord = getRandomWord()
-    setWord(randomWord)
+    setWord(getRandomWord())
   }, [])
-  const [word, setWord] = React.useState("")
+
+  React.useEffect(() => {
+    if (attempts <= 0) {
+      setGameOver(true)
+      setShowWord(true)
+    } else if (correctLetters.length === new Set(word.split("")).size) {
+      setGameWon(true)
+      setShowWord(true)
+    }
+  }, [attempts, correctLetters, word])
+
+  React.useEffect(() => {
+    if (startGame) {
+      setWord(getRandomWord())
+      setAttempts(6)
+      setGuessedLetters([])
+      setCorrectLetters([])
+      setWrongLetters([])
+      setGameOver(false)
+      setGameWon(false)
+      setShowWord(false)
+    }
+  }, [startGame])
+
+  React.useEffect(() => {
+    if (gameOver) {
+      alert("Game Over! You lost!")
+    } else if (gameWon) {
+      alert("Congratulations! You won!")
+    }
+  }, [gameOver, gameWon])
 
   return (
     <Layout>
@@ -30,8 +68,51 @@ function Hangman() {
         <h1 className="GameTitle">Hangman</h1>
         <p className="GameDescription">A fun word guessing game!</p>
         <p>Guess the word before you run out of attempts!</p>
+        <div className="gameContainer">
+          <div className="wordContainer">
+            {word.split("").map((letter, index) => (
+              <span key={index} className="letter">
+                {guessedLetters.includes(letter) ? letter : "_"}
+              </span>
+            ))}
+          </div>
+          <div className="attemptsContainer">
+            <p>Attempts left: {attempts}</p>
+          </div>
+          <div className="lettersContainer">
+            {guessedLetters.map((letter, index) => (
+              <span key={index} className="guessedLetter">
+                {letter}
+              </span>
+            ))}
+          </div>
+          <div className="inputContainer">
+            <input
+              type="text"
+              maxLength="1"
+              onChange={(e) => {
+                const letter = e.target.value.toLowerCase()
+                if (letter && !guessedLetters.includes(letter)) {
+                  setGuessedLetters([...guessedLetters, letter])
+                  if (word.includes(letter)) {
+                    setCorrectLetters([...correctLetters, letter])
+                  } else {
+                    setWrongLetters([...wrongLetters, letter])
+                    setAttempts(attempts - 1)
+                  }
+                }
+                e.target.value = ""
+              }}
+            />
+          </div>
+        </div>
+        <div className="gameControls">
+          <button onClick={() => setStartGame(true)}>Start Game</button>
+          <button onClick={() => setStartGame(true)}>Restart Game</button>
+        </div>
       </div>
     </Layout>
   )
 }
+
 export default Hangman
